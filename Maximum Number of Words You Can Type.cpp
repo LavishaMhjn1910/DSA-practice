a263a37df171366e6b66eb8a5ad89546137b701d
Lavisha Mahajan <Lavishamahajan2005@gmail.com>
2025-09-15 17:45:31 +0530

    Implement function to count typeable words

---
+++ Maximum Number of Words You Can Type.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        unordered_set<char> broken(brokenLetters.begin(), brokenLetters.end());
+        int count = 0;
+        stringstream ss(text);
+        string word;
+        
+        while (ss >> word) {
+            bool valid = true;
+            for (char c : word) {
+                if (broken.count(c)) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) count++;
+        }
+        
+        return count;
+    }
+};
